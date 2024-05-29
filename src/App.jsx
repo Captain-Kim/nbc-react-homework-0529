06ba0216f71fa3b13ea9a10bf89770130dcbd8da
Captain-Kim <brighthero@kakao.com>
2024-05-29 09:19:37 +0900

    Router 분리 누락 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { DataProvider } from './DataContext';
-
+import Router from './Router/Router.jsx';
 
 function App() {
   return (
     <>
       <DataProvider>
-        <AppContent />
+        <Router />
       </DataProvider>
     </>
   );
