a1e7a0b44897a9be765170660ebc52471295c966
Captain-Kim <brighthero@kakao.com>
2024-05-29 09:15:46 +0900

    fix: Router 컴포넌트 구분

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { DataProvider } from './DataContext';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
 
-function AppContent() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/detail" element={<Detail />} />
-    </Routes>
-  );
-}
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <DataProvider>
         <AppContent />
       </DataProvider>
-    </BrowserRouter>
+    </>
   );
 }
 
